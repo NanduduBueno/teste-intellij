e3b7e7b70567faf1793f12578efd01f97db3c0e8
NanduduBueno <eduardo.eodab@gmail.com>
2023-03-22 01:51:04 -0300

    Commit 2

--- src/dio/teste.java
+++ src/dio/teste.java
@@ -2,6 +2,6 @@ package dio;
 
 public class teste {
     public static void main(String[] args) {
-        System.out.println("flamengo rubro negro");
+        System.out.println("flamengo rubro negro vencer");
     }
 }
